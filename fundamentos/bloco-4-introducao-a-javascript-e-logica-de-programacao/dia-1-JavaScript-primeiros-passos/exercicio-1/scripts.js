78328d047d57a8c66f27854632858e1c64feb46b
Matheus Simplicio <matheusschulz47@gmail.com>
2022-08-18 16:30:46 -0300

    Alterando valor de algumas variaveis

--- fundamentos/bloco-4-introducao-a-javascript-e-logica-de-programacao/dia-1-JavaScript-primeiros-passos/exercicio-1/scripts.js
+++ fundamentos/bloco-4-introducao-a-javascript-e-logica-de-programacao/dia-1-JavaScript-primeiros-passos/exercicio-1/scripts.js
@@ -1,5 +1,7 @@
 const myName = 'matheus';
 const birthCity = 'joão pessoa';
-let birthYear = '1999'  
+let birthYear = 1999; 
 console.log(myName, birthCity)
-
+birthYear = 2030;
+console.log(birthYear)
+birthCity = 'são paulo' //nao vai funcionar pois não se altera valor de const.
