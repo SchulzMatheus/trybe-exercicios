bbcc95f18d1347cdaceebfe0291fa7ca3c5c78ca
Matheus Simplicio <matheusschulz47@gmail.com>
2022-08-18 22:45:46 -0300

    teste 1

---
+++ fundamentos/bloco-4-introducao-a-javascript-e-logica-de-programacao/dia-1-JavaScript-primeiros-passos/exercicio-3/scripts.js
@@ -0,0 +1,15 @@
+const nota = 79;
+if(nota>=80)
+{
+    console.log("Parabéns, você foi aprovado(a)!");
+}
+else if(nota<80 && nota>=60){
+
+    console.log ("voce está na lista de espera");
+}
+else if(nota<60){
+    console.log("voce foi reprovado!");
+}
+else {
+    console.log("Dados invalidos!!");
+}
