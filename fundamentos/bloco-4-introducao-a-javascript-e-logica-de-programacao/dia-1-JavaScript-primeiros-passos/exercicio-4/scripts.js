e7d4c12522ef144a9918676ded4cf2cc519a7d37
Matheus Simplicio <matheusschulz47@gmail.com>
2022-08-18 22:56:36 -0300

    criando as primeiras duas condições

--- fundamentos/bloco-4-introducao-a-javascript-e-logica-de-programacao/dia-1-JavaScript-primeiros-passos/exercicio-4/scripts.js
+++ fundamentos/bloco-4-introducao-a-javascript-e-logica-de-programacao/dia-1-JavaScript-primeiros-passos/exercicio-4/scripts.js
@@ -1,2 +1,10 @@
 const currentHour = 6;
 let message = '';
+
+if (currentHour>=22){
+    message = 'Não deveríamos comer nada, é hora de dormir';
+    console.log(message);
+}
+else if(currentHour>=18 && currentHour<22){
+    message = 'Rango da noite, vamos jantar :D'
+}
