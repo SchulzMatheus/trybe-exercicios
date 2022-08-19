ddb0c7b3889f05f11138fda2188215c8fd46a774
Matheus Simplicio <matheusschulz47@gmail.com>
2022-08-18 23:31:30 -0300

    testes completos

--- fundamentos/bloco-4-introducao-a-javascript-e-logica-de-programacao/dia-1-JavaScript-primeiros-passos/exercicio-4/ex-2/scripts.js
+++ fundamentos/bloco-4-introducao-a-javascript-e-logica-de-programacao/dia-1-JavaScript-primeiros-passos/exercicio-4/ex-2/scripts.js
@@ -1,10 +1,15 @@
-let weekDay = 'terça-feira';
-if(weekDay = 'segunda-feira' || 'terça-feira' || 'quarta-feira' || 'quinta-feira' || 'sexta-feira'){
-   console.log("Oba, mais um dia de aprendizado na Trybe >:D");
+let weekDay = 'x';
+
+if(weekDay == ('segunda-feira' || 'terça-feira' || 
+'quarta-feira' || 'quinta-feira' || 'sexta-feira')){
+
+   console.log("Oba, mais um dia de aprendizado na Trybe ")
+
 }
-else if (weekDay = 'domingo' || 'sábado'){
+
+else if (weekDay == ('domingo' || 'sábado')){
     console.log("FINALMENTE, descanso merecido UwU");
 }
 else {
-    console.log("Entrada invalida");
+    console.log("Entrada errada!")
 }
