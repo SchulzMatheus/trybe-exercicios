b3296f8521814aa9b1718ab0d72921098d47801d
Matheus Simplicio <matheusschulz47@gmail.com>
2022-08-19 00:50:56 -0300

    finalizando o programa 1

---
+++ fundamentos/bloco-4-introducao-a-javascript-e-logica-de-programacao/dia-1-JavaScript-primeiros-passos/exercicio-final/pg-1/scripts.js
@@ -0,0 +1,17 @@
+const a = 8;
+const b = 9;
+console.log("adição")
+x = a+b;
+console.log(x);
+console.log("Subtração")
+x = a-b;
+console.log(x);
+console.log("Multiplicação")
+x = a*b;
+console.log(x);
+console.log("Divisão")
+x = a/b;
+console.log(x);
+console.log("Modulo")
+x = a%b;
+console.log(x);
