e83b0378667561da6237f646305943190246c779
Matheus Simplicio <matheusschulz47@gmail.com>
2022-08-18 23:09:29 -0300

    Lógica do ex 2 feitas

---
+++ fundamentos/bloco-4-introducao-a-javascript-e-logica-de-programacao/dia-1-JavaScript-primeiros-passos/exercicio-4/ex-2/scripts.js
@@ -0,0 +1,10 @@
+let weekDay = 'quarta-feira';
+if(weekDay = 'segunda-feira' || 'terça-feira' || 'quarta-feira' || 'quinta-feira' || 'sexta-feira'){
+   console.log("Oba, mais um dia de aprendizado na Trybe >:D");
+}
+else if (weekDay = 'domingo' || 'sábado'){
+    console.log("FINALMENTE, descanso merecido UwU");
+}
+else {
+    console.log("Entrada invalida");
+}
