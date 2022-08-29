fdfe43094c6398d529a3ea7a93c4cb71bd6fd278
Matheus Simplicio <matheusschulz47@gmail.com>
2022-08-29 19:26:41 -0300

    Fazendo a ordem crescente

---
+++ fundamentos/bloco-4-introducao-a-javascript-e-logica-de-programacao/dia-2-javascript-array-e-loop-for/exercicio-bonus/ordemCrescente.js
@@ -0,0 +1,13 @@
+let numbers = [5, 9, 3, 19, 70, 8, 100, 2, 35, 27];
+let x;
+
+for (let i = 0; i<numbers.length; i++){
+    for(let i2 = 0; i2<(numbers.length-i-1); i2++){
+        if(numbers[i2]>numbers[i2+1]){
+            x = numbers[i2]
+            numbers[i2] = numbers[i2+1]
+            numbers[i2+1] = x
+        }
+    }
+}
+console.log(numbers)
