35cab5e6207c3515fdd112789f3a9a8b5ecc794e
Matheus Simplicio <matheusschulz47@gmail.com>
2022-08-29 18:46:01 -0300

    script 5 finalizado

---
+++ fundamentos/bloco-4-introducao-a-javascript-e-logica-de-programacao/exercicio-2/script5.js
@@ -0,0 +1,12 @@
+let numbers = [5, 9, 3, 19, 70, 8, 100, 2, 35, 27];
+let maiorNumero = 0;
+for(let i = 0; i < numbers.length; i++)
+{
+    if(maiorNumero<numbers[i])
+    {
+        maiorNumero = numbers[i]
+    }
+
+}
+ console.log(maiorNumero)
+
