35909435f04fac442235d88e3cd879ac46376980
Matheus Simplicio <matheusschulz47@gmail.com>
2022-08-30 19:34:21 -0300

    triangulo invertido

--- fundamentos/bloco-4-introducao-a-javascript-e-logica-de-programacao/dia-3-javascript-logica-de-programacao-e-algoritmos/exercicio-5/scripts.js
+++ fundamentos/bloco-4-introducao-a-javascript-e-logica-de-programacao/dia-3-javascript-logica-de-programacao-e-algoritmos/exercicio-5/scripts.js
@@ -6,7 +6,7 @@ for (let linha = 1; linha <= a; linha++) {
   }
   console.log(b);
   b = "";
-}*/
+}
 
 /*let a = 5;
 let b = '';
@@ -17,6 +17,18 @@ for (let linha = 1; linha <= a; linha++) {
         }
     }
     console.log(b)
- } */
+ } 
 
-
+   */ let base = 5
+    
+    for(let i = 0; i<base; i++){
+      let a = '';
+      for(let j = 1; j<base - i; j++) {
+        a += ' ';
+      }
+      for(let j = 0; j<i + 1; j++){
+        a += '*'
+      }
+      console.log(a)
+    }
+  
