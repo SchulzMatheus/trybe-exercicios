8f52b4cfe893457c7020b29dd748ba5fb388bcb6
Matheus Simplicio <matheusschulz47@gmail.com>
2022-08-30 18:28:10 -0300

    quadrado e triangulo

---
+++ fundamentos/bloco-4-introducao-a-javascript-e-logica-de-programacao/dia-3-javascript-logica-de-programacao-e-algoritmos/exercicio-5/scripts.js
@@ -0,0 +1,22 @@
+/*let a = 5;
+let b = "";
+for (let linha = 1; linha <= a; linha++) {
+  for (let coluna = 1; coluna <= a; coluna++) {
+    b += "*";
+  }
+  console.log(b);
+  b = "";
+}*/
+
+/*let a = 5;
+let b = '';
+for (let linha = 1; linha <= a; linha++) {
+    for(let coluna = 1; coluna <= a; coluna++) {
+        if (coluna == linha) {
+            b += '*'
+        }
+    }
+    console.log(b)
+ } */
+
+
