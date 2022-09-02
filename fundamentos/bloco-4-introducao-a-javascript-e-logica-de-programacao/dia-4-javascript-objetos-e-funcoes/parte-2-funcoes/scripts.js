42e6698eadd4278f0fd4289a1755c224d4fb3979
Matheus Simplicio <matheusschulz47@gmail.com>
2022-09-01 22:27:03 -0300

    terminando a função 4

--- fundamentos/bloco-4-introducao-a-javascript-e-logica-de-programacao/dia-4-javascript-objetos-e-funcoes/parte-2-funcoes/scripts.js
+++ fundamentos/bloco-4-introducao-a-javascript-e-logica-de-programacao/dia-4-javascript-objetos-e-funcoes/parte-2-funcoes/scripts.js
@@ -40,6 +40,30 @@ function lowestCount(arrayofNumbers){
     }
     return x
 }
-
+function highestWord(array) {
+    let a = []
+    let b = []
+    
+    for(let i = 0; i<array.length; i++)
+    {
+        a = array[i].split("");
+        b.push(a.length) 
+    }
+    let maiorNumero = b.length
+    
+    let xz = [];
+    for(let i = 0; i<b.length; i++)
+    {
+        if(maiorNumero<b[i])
+        {
+   
+            xz.push(array[i])
+        }
+    }
+    
+    return xz;
+}
+let tu = ['José', 'Lucas', 'Nádia', 'Fernanda', 'Cairo', 'Joana'];
+console.log(highestWord(tu))
 
 
