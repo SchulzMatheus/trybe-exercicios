7c4b757d213e2fb21c6cf01ea7c0770d84ba2b9b
Matheus Simplicio <matheusschulz47@gmail.com>
2022-09-03 22:21:24 -0300

    mudando algumas styles

--- fundamentos/bloco-5-javascript-dom-eventos-e-web-storage/dia-1-javascript-dom-e-seletores/exercicio-2/script.js
+++ fundamentos/bloco-5-javascript-dom-eventos-e-web-storage/dia-1-javascript-dom-e-seletores/exercicio-2/script.js
@@ -2,6 +2,9 @@ const body = document.body
 const urgente = document.getElementsByTagName('section')
 const h3 = document.getElementsByTagName('h3')
 const div = document.getElementsByTagName('div')
+document.getElementsByTagName('h1')[0].style.color = 'black'
+document.getElementsByTagName('footer')[0].style.color = 'green'
+
 body.style.background = '#d9aefc';
 for(let i = 0; i<2; i++){
     h3[i].style.background = '#85a5fc'
@@ -13,7 +16,6 @@ for(let i = 2; i<4; i++){
 for(let i = 0; i<h3.length; i++)
 {
     h3[i].style.color = 'black'
-    h3[i]
 }
 for(let i = 0; i<urgente.length; i++)
 {
