73ee9d276a3c18e74a8b202145a5559e67943ef8
Matheus Simplicio <matheusschulz47@gmail.com>
2022-09-05 00:00:33 -0300

    adicionando um texto a o elemento filho do filho

--- fundamentos/bloco-5-javascript-dom-eventos-e-web-storage/exercicio-1/script.js
+++ fundamentos/bloco-5-javascript-dom-eventos-e-web-storage/exercicio-1/script.js
@@ -1,3 +1,4 @@
 // arquivo script.js
 console.log(document.getElementById('elementoOndeVoceEsta'))
 document.getElementById('elementoOndeVoceEsta').parentElement.style.color = 'blue'
+document.getElementById('elementoOndeVoceEsta').firstElementChild.innerHTML = 'Pão'
