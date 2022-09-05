90bfb5efdd7f05193b7989cb2972b05b7f764da1
Matheus Simplicio <matheusschulz47@gmail.com>
2022-09-05 02:52:24 -0300

    criando filho da tag main, atribuindo uma classe e criando um paragrafo dentro deste filho

--- fundamentos/bloco-5-javascript-dom-eventos-e-web-storage/script.js
+++ fundamentos/bloco-5-javascript-dom-eventos-e-web-storage/script.js
@@ -1,11 +1,17 @@
 let h1 = document.createElement('h1')
 let main = document.createElement('main')
 let section = document.createElement('section')
+let p = document.createElement('p')
 document.body.appendChild(h1)
 document.body.appendChild(main)
 document.getElementsByTagName('h1')[0].innerHTML = 'Exercício 5.2 - JavaScript DOM'
 document.getElementsByTagName('h1')[0].style.textAlign = 'center'
 document.getElementsByTagName('main')[0].className = 'main-content'
+document.getElementsByClassName('main-content')[0].appendChild(section)
+document.getElementsByClassName('main-content')[0].firstChild.className = 'center-content'
+document.getElementsByClassName('center-content')[0].appendChild(p)
+
+
 
 
 
