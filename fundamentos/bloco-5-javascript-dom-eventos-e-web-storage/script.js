ebaaf54994ef764f5f13e792c506e14004d972fe
Matheus Simplicio <matheusschulz47@gmail.com>
2022-09-05 02:36:36 -0300

    criando h1 e adicionando texto

---
+++ fundamentos/bloco-5-javascript-dom-eventos-e-web-storage/script.js
@@ -0,0 +1,4 @@
+let h1 = document.createElement('h1')
+document.body.appendChild(h1)
+document.getElementsByTagName('h1')[0].innerHTML = 'Exercício 5.2 - JavaScript DOM'
+document.getElementsByTagName('h1')[0].style.textAlign = 'center'
