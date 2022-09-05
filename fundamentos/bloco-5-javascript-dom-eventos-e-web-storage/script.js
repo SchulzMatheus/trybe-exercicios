d16e31b63ca4c90896c34c136244ae6b4c823b6c
Matheus Simplicio <matheusschulz47@gmail.com>
2022-09-05 02:43:05 -0300

    criando a tag main

--- fundamentos/bloco-5-javascript-dom-eventos-e-web-storage/script.js
+++ fundamentos/bloco-5-javascript-dom-eventos-e-web-storage/script.js
@@ -1,4 +1,12 @@
 let h1 = document.createElement('h1')
+let main = document.createElement('main')
+let section = document.createElement('section')
 document.body.appendChild(h1)
+document.body.appendChild(main)
 document.getElementsByTagName('h1')[0].innerHTML = 'Exercício 5.2 - JavaScript DOM'
 document.getElementsByTagName('h1')[0].style.textAlign = 'center'
+document.getElementsByTagName('main')[0].className = 'main-content'
+
+
+
+
