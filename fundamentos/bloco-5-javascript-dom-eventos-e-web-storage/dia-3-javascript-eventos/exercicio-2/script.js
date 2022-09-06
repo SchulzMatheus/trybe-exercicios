7b0b29cc0a0b2f66ad811d5ecbf84153214de98b
Matheus Simplicio <matheusschulz47@gmail.com>
2022-09-05 23:19:38 -0300

    adicionando classes friday e holiday

--- fundamentos/bloco-5-javascript-dom-eventos-e-web-storage/dia-3-javascript-eventos/exercicio-2/script.js
+++ fundamentos/bloco-5-javascript-dom-eventos-e-web-storage/dia-3-javascript-eventos/exercicio-2/script.js
@@ -15,14 +15,31 @@ function createDaysOfTheWeek() {
   
   // Escreva seu código abaixo.
 let decemberDaysList = [29, 30, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20, 21, 22, 23, 24, 25, 26, 27, 28, 29, 30, 31];
+const dezembroFeriados = [24, 25, 31];
+const sextaDezembro = [4, 11, 18, 25]
 let ul = document.getElementById('days')
 
 for(let i = 0; i<decemberDaysList.length; i++){
     const li = document.createElement('li')
+    const days = decemberDaysList[i]
+    li.className = 'day'
+    li.innerText = days
+    for(let index = 0; index<dezembroFeriados.length; index++)
+    {
+        if (days === dezembroFeriados[index]){
+            li.classList.add('holiday')
+        }
+    }
+    for(let index = 0; index<sextaDezembro.length; index++)
+    {
+        if (days === sextaDezembro[index]){
+            li.classList.add('friday')
+        }
+    }
     ul.appendChild(li)
-    ul.lastElementChild.className = 'day'
-    ul.lastElementChild.innerHTML = decemberDaysList[i];
+    
 }
+
 function createFeriado(string)
 {
    const newButton = document.createElement('button')
