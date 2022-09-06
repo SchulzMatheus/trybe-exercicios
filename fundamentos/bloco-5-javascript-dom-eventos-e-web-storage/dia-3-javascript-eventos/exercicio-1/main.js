58dd2230c75d3593f88d09758ba09a2e3a41f32a
Matheus Simplicio <matheusschulz47@gmail.com>
2022-09-05 21:02:36 -0300

    adicionando funcao que ao dar db em cima do "meu top 3" redireciona para o github

--- fundamentos/bloco-5-javascript-dom-eventos-e-web-storage/dia-3-javascript-eventos/exercicio-1/main.js
+++ fundamentos/bloco-5-javascript-dom-eventos-e-web-storage/dia-3-javascript-eventos/exercicio-1/main.js
@@ -22,13 +22,13 @@ function textEdit(event){
     const tech = elementTech[0]
     tech.innerHTML = newText
 }
+
 input.addEventListener('input', textEdit)
 
-
-
-
-// 3. Crie uma função que, ao digitar na caixa de texto, altere o texto do elemento
-// com a classe 'tech';
+function newWindow(){
+    window.open("https://github.com/SchulzMatheus", "_blank")
+}
+myWebpage.addEventListener('dblclick', newWindow)
 
 // 4. Crie uma função que, ao clicar duas vezes em 'Meu top 3 do Spotrybefy', ele
 // redirecione para alguma página;
