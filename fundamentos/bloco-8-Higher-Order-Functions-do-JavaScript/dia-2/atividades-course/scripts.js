e1f91d7550e89b26daa92a963030e1f3d889e446
Matheus Simplicio <matheusschulz47@gmail.com>
2022-09-23 16:04:16 -0300

    exemplo toFind

---
+++ fundamentos/bloco-8-Higher-Order-Functions-do-JavaScript/dia-2/atividades-course/scripts.js
@@ -0,0 +1,111 @@
+// const students = [
+//     { name: 'Maria', grade: 70, approved: '' },
+//     { name: 'José', grade: 56, approved: '' },
+//     { name: 'Roberto', grade: 90, approved: '' },
+//     { name: 'Ana', grade: 81, approved: '' },
+//   ];
+  
+//   function verifyGrades() {
+//     for (let index = 0; index < students.length; index += 1) {
+//       const student = students[index];
+//       if (student.grade >= 60) {
+//         student.approved = 'Aprovado';
+//       } else {
+//         student.approved = 'Recuperação';
+//       }
+//     }
+//   }
+  
+//   verifyGrades();
+  
+//   console.log(students);
+  // [
+  //   { name: 'Maria', grade: 70, approved: 'Aprovado' },
+  //   { name: 'José', grade: 56, approved: 'Recuperação' },
+  //   { name: 'Roberto', grade: 90, approved: 'Aprovado' },
+  //   { name: 'Ana', grade: 81, approved: 'Aprovado' }
+  // ]
+//   const students = [
+//     { name: 'Maria', grade: 70, approved: '' },
+//     { name: 'José', grade: 56, approved: '' },
+//     { name: 'Roberto', grade: 90, approved: '' },
+//     { name: 'Ana', grade: 81, approved: '' },
+//   ];
+  
+//   function verifyGrades() {
+//     students.forEach((student, index) => {
+//       if (student.grade >= 60) {
+//         students[index].approved = 'Aprovado';
+//       } else {
+//         students[index].approved = 'Recuperação';
+//       }
+//     });
+//   }
+  
+//   verifyGrades();
+  
+//   console.log(students);
+  // [
+  //   { name: 'Maria', grade: 70, approved: 'Aprovado' },
+  //   { name: 'José', grade: 56, approved: 'Recuperação' },
+  //   { name: 'Roberto', grade: 90, approved: 'Aprovado' },
+  //   { name: 'Ana', grade: 81, approved: 'Aprovado' }
+  // ]
+//   const numbers = [11, 24, 39, 47, 50, 62, 75, 81, 96, 100];
+// let firstMultipleOf5;
+// for (let index = 0; index < numbers.length; index += 1) {
+//   if (numbers[index] % 5 === 0) {
+//     firstMultipleOf5 = numbers[index];
+//     break;
+//   }
+// }
+
+// console.log(firstMultipleOf5);
+// 50
+// const numbers = [11, 24, 39, 47, 50, 62, 75, 81, 96, 100];
+// const firstMultipleOf5 = numbers.find((number) => number % 5 === 0);
+
+// console.log(firstMultipleOf5);
+// // 50
+// const arrayOfValues = ['josé', 50, 0.25, { comida: 'Chocolate' }];
+// arrayOfValues.forEach((element) => {
+//   console.log('Cada elemento do array:', element);
+// });
+
+// Cada elemento do array: josé
+// Cada elemento do array: 50
+// Cada elemento do array: 0.25
+// Cada elemento do array: { comida: 'Chocolate' }
+// const arrayOfValues = ['josé', 50, 0.25, { comida: 'Chocolate' }];
+// arrayOfValues.forEach((element, indexOfTheArray, theEntireArray) => {
+//   console.log('Cada elemento do array:', element);
+//   console.log('Index, posição do elemento:', indexOfTheArray);
+//   console.log('Array percorrido:', theEntireArray);
+// });
+
+// => ---------------
+//   Cada elemento do array: josé
+//   Index, posição do elemento: 0
+//   Array percorrido: [ 'josé', 50, 0.25, { comida: 'Chocolate' } ]
+//   ---------------
+//   Cada elemento do array: 50
+//   Index, posição do elemento: 1
+//   Array percorrido: [ 'josé', 50, 0.25, { comida: 'Chocolate' } ]
+//   ---------------
+//   Cada elemento do array: 0.25
+//   Index, posição do elemento: 2
+//   Array percorrido: [ 'josé', 50, 0.25, { comida: 'Chocolate' } ]
+//   ---------------
+//   Cada elemento do array: { comida: 'Chocolate' }
+//   Index, posição do elemento: 3
+//   Array percorrido: [ 'josé', 50, 0.25, { comida: 'Chocolate' } ]
+
+const numbers = [19, 21, 30, 3, 45, 22, 15];
+
+const findDivisibleBy3And5 = (number) => {
+  if(number % 5 === 0 && number % 3 === 0) return true; else return false;
+};
+
+const isDivisible = numbers.find(findDivisibleBy3And5);
+
+console.log(isDivisible);
