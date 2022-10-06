a277bdfc95dc58aee434bda48636e5518a26b380
Matheus Simplicio <matheusschulz47@gmail.com>
2022-10-06 16:19:58 -0300

    x

--- front-end/Secao-1-JavaScript-e-Testes-Assincronos/dia-1-JavaScript-Assincrono-e-Callbacks/exerCourse/exe-3.js
+++ front-end/Secao-1-JavaScript-e-Testes-Assincronos/dia-1-JavaScript-Assincrono-e-Callbacks/exerCourse/exe-3.js
@@ -1,50 +1,35 @@
-// // callback-errors.js
+// callback-errors.js
 
-// const countryName = ({ name }) => console.log(`O país retornado é ${name}`);
-// const countryCurrency = ({ name, currency }) => console.log(`A Moeda corrente do ${name} é ${currency}`);
+const countryName = ({ name }) => console.log(`O país retornado é ${name}`);
+const countryCurrency = ({ name, currency }) => console.log(`A Moeda corrente do ${name} é ${currency}`);
 
-// const delay = (maxMilliseconds = 5000) => Math.floor(Math.random() * maxMilliseconds);
+const delay = (maxMilliseconds = 5000) => Math.floor(Math.random() * maxMilliseconds);
 
-// const printErrorMessage = (error) => console.log(`Erro ao selecionar país: ${error}`);
+const printErrorMessage = (error) => console.log(`Erro ao selecionar país: ${error}`);
 
-// const getCountry = (onSuccess, onFail) => {
-//   setTimeout(() => {
-//     const didOperationSucceed = Math.random() >= 0.5;
-//     if (didOperationSucceed) {
-//       const country = {
-//         name: 'Brasil',
-//         hdi: 0.759,
-//         currency: 'Real',
-//       };
-//       onSuccess(country);
-//     } else {
-//       const errorMessage = 'País não encontrado';
-//       onFail(errorMessage);
-//     }
-//   }, delay());
-// };
+const getCountry = (onSuccess, onFail) => {
+  setTimeout(() => {
+    const didOperationSucceed = Math.random() >= 0.5;
+    if (didOperationSucceed) {
+      const country = {
+        name: 'Brasil',
+        hdi: 0.759,
+        currency: 'Real',
+      };
+      onSuccess(country);
+    } else {
+      const errorMessage = 'País não encontrado';
+      onFail(errorMessage);
+    }
+  }, delay());
+};
 
-// // Deve imprimir:
-// //  - Em caso de sucesso: "O país retornado é Brasil"
-// //  - Em caso de erro: "Erro ao selecionar país: País não encontrado"
-// getCountry(countryName, printErrorMessage);
+// Deve imprimir:
+//  - Em caso de sucesso: "O país retornado é Brasil"
+//  - Em caso de erro: "Erro ao selecionar país: País não encontrado"
+getCountry(countryName, printErrorMessage);
 
-// // Deve imprimir:
-// //  - Em caso de sucesso: "A moeda corrente do Brasil é Real"
-// //  - Em caso de erro: "Erro ao selecionar país: País não encontrado"
-// getCountry(countryCurrency, printErrorMessage);
-
-let timeA = 1000;
-let timeB = 500;
-let numA = 4;
-let numB = 6;
-let numC = 2;
-const sum = (a, b) => a + b;
-setTimeout(() => {
-  numC = sum(numA, numB);
-  console.log(numC);
-}, timeA);
-console.log(numC);
-setTimeout(() => {
-  console.log(sum(numA, numC) === numB);
-}, timeB);
+// Deve imprimir:
+//  - Em caso de sucesso: "A moeda corrente do Brasil é Real"
+//  - Em caso de erro: "Erro ao selecionar país: País não encontrado"
+getCountry(countryCurrency, printErrorMessage);
